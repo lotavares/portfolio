3910b09eef1b722d2c5e12d64184cf0a25c7af21
Lorena Tavares <tavares.loren@gmail.com>
2025-09-18 07:38:25 -0400

    Update experience

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,7 +6,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Lorena Tavares | Portfolio",
-  description: "Software Engineer",
+  description: "Software Developer",
   icons: {
     icon: '/favicon.svg',
   },
